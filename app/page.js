9a235b170f3e45c849b06f4b65a87cc66fbde7ca
chidam <chidam3chain@gmail.com>
2023-08-23 01:52:57 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -16,24 +16,29 @@ import el from '../images/el.png'
 import dl from '../images/dl.png'
 import about from '../images/about.png'
 import { useState } from 'react'
+import "./style.css"
 export default function Home() {
   const [open, setopen] = useState(true)
   return (
     <>
       <div className='font-sans scroll-smooth'>
-        <nav className="bg-white shadow-md p-4 s md:flex flex-row justify-between items-center relative z-0">
+        <nav className="bg-white shadow-md p-4 s md:flex flex-row justify-between items-center relative z-0 sm:[20vh] md:h-[16vh]">
           <div className="flex flex-row justify-between">
-            <Image className='md:w-96 w-48' src={tbi} />
+            <div className='images grid w-60 md:flex lg:w-80 md:w-60'>
+              <Image className='md:w-36' src={sas} />
+              <Image className='' src={tbi} />
+            </div>
             <span className="md:hidden block cursor-pointer"><button onClick={() => setopen(!open)}  ><Image className="w-8" src={ham} /></button></span>
           </div>
 
 
 
-          <ul className={`top-16 transition-all ease-in font-bold border-x-2 border-b-2 md:border-none border-orange-300 duration-300 ${open ? 'opacity-0 md:opacity-100' : ''} bg-orange-100   md:py-0 pt-4 pl-2 md:pl-0 w-full md:w-auto left-0 md:flex flex-row md:items-center  z-[-1] md:z-auto md:static absolute md:bg-transparent`}>
-            <li className="mx-4 md:my-0 my-6 pb-2 border-b-2 md:border-none border-orange-300 cursor-pointer hover:text-orange-600"><a href='#about'>About</a></li>
-            <li className="mx-4 md:my-0 my-6 pb-2 border-b-2 md:border-none border-orange-300 cursor-pointer hover:text-orange-600"><a href='#eligible'>Eligibility</a></li>
+          <ul className={`top-36 transition-all ease-in font-bold border-x-2 border-b-2 md:border-none border-orange-300 duration-300 ${open ? 'opacity-0 md:opacity-100' : ''} bg-orange-100   md:py-0 pt-4 pl-2 md:pl-0 w-full md:w-auto left-0 md:flex flex-row md:items-center  z-[-1] md:z-auto md:static absolute md:bg-transparent`}>
+            {/* <li className="mx-4 md:my-0 my-6 pb-2 border-b-2 md:border-none border-orange-300 cursor-pointer hover:text-orange-600"><a href='#about'>About</a></li> */}
+            {/* <li className="mx-4 md:my-0 my-6 pb-2 border-b-2 md:border-none border-orange-300 cursor-pointer hover:text-orange-600"><a href='#eligible'>Eligibility</a></li> */}
+            <li className="mx-4 md:my-0 my-6 pb-2 border-b-2 md:border-none border-orange-300 cursor-pointer hover:text-orange-600"><a href='#details'>Details</a></li>
             <li className="mx-4 md:my-0 my-6 pb-2 border-b-2 md:border-none border-orange-300 cursor-pointer hover:text-orange-600"><a href='#idea'>Idea/Themes</a></li>
-            <li className="mx-4 md:my-0 my-2 pb-2  md:border-none border-orange-300 cursor-pointer hover:text-orange-600"><a href='#awa'>Date/Awards</a></li>
+            {/* <li className="mx-4 md:my-0 my-2 pb-2  md:border-none border-orange-300 cursor-pointer hover:text-orange-600"><a href='#awa'>Date/Awards</a></li> */}
 
 
 
@@ -43,22 +48,18 @@ export default function Home() {
 
         {/* hero */}
 
-        <div className='flex  justify-center item-center flex-col sm:px-4 md:pt-8 md:px-12 pt-6 bg-gradient-to-tr from-orange-200 '>
+        <div className='flex  justify-center item-center flex-col sm:px-4 md:pt-8 md:px-12 pt-6 bg-gradient-to-tr from-orange-200 h-[80vh] md:h-[84vh]'>
           <div className='flex md:flex-row flex-col justify-center items-center '>
             <div className='md:w-1/3 flex flex-col space-y-4 md:space-y-6 '>
               <div className='md:text-4xl md:text-left text-center lg:text-3xl 2xl:text-7xl xl:text-5xl text-4xl md:ml-0 ml-4 font-bold '>Nutrition Hackathon <br></br> 2023</div>
               <div className='md:text-xl  lg:text-2xl 2xl:text-4xl text-2xl text-center md:text-left md:ml-0 ml-4'>Seeking creative theme-oriented ideas from creators throughout india</div>
               <div className='md:py-4  py-4 md:ml-0 ml- flex md:justify-start justify-center'>
-                <button className='px-4 md:px-6 lg:px-8 p-2 rounded-3xl flex text-xl md:text-xl flex-row justify-center items-center space-x-2 hover:bg-orange-300 bg-orange-400'><span><a href='https://docs.google.com/forms/d/e/1FAIpQLSeVsVsw2P336oIxawTvst0Gkazk0q82MBergJymdz9_8qZOZA/viewform' target='_main'>Register</a></span> <Image className='w-4' src={arrow} /></button>
+                <button className='px-4 md:px-6 lg:px-8 p-2 rounded-3xl flex text-xl md:text-xl flex-row justify-center items-center space-x-2 hover:bg-orange-300 bg-orange-400'><span><a id='details' href='https://docs.google.com/forms/d/e/1FAIpQLSeVsVsw2P336oIxawTvst0Gkazk0q82MBergJymdz9_8qZOZA/viewform' target='_main'>Register</a></span> <Image className='w-4' src={arrow} /></button>
               </div>
             </div>
-            <div className='md:w-1/3 '>
-              <Image className='' src={fruits} />
+            <div className='md:block'>
+              <Image className='h-[30vh] w-[30vh] md:h-[50vh] md:w-[50vh]' src={fruits} />
             </div>
-          </div>
-
-          <div className='flex bg-transparent justify-center my-6 md:my-12 items-center'>
-            <div className='w-2/3  h-1 bg-gray-500'></div>
           </div>
 
         </div>
@@ -67,7 +68,7 @@ export default function Home() {
         {/* about */}
 
 
-        <div id='about' className='flex flex-col bg-gradient-to-br from-orange-200 space-y-4 pb-8 justify-center items-center pt-8 p-4'>
+        {/* <div id='about' className='flex flex-col bg-gradient-to-br from-orange-200 space-y-4 pb-8 justify-center items-center pt-8 p-4'>
           <div className='lg:text-4xl font-bold text-3xl md:text-3xl'>About</div>
           <Image className='w-72' src={about} />
           <div className='md:w-3/5 text-center font-semibold text-xl md:text-2xl'>We are welcoming submissions for theme-based concepts in the "Nutrition Hackathon 2023," calling upon Students, Research Scholars, Faculty, and Innovators from across India to participate.
@@ -87,13 +88,40 @@ export default function Home() {
           
 
 
+        </div> */}
+        
+        {/* details */}
+        <div className='details h-[150vh] lg:h-[60vh] relative'>
+          <div className='cardcontain flex lg:flex-row flex-col justify-center items-center gap-24 lg:gap-6'>
+            <div className='card -mt-[10vh] pt-6 pl-4 pr-6 relative bg-slate-200/90 transition-transform rounded-xl hover:scale-105 hover:shadow-2xl  h-[55vh] w-[80vw] lg:w-[25vw]'>
+                {/* <div className="blur w-full h-full absolute top-0 bg-white"></div> */}
+                <div className="image mb-4 h-[20vh] w-full grid place-items-center">
+                <Image src={el} className='h-[20vh] w-[25vh]'/>
+                </div>
+                <div className='heading  font-bold space text-3xl mb-6'>Eligibility</div>
+                <div className='content text-xl'>Students, Research Scholars, Faculties & Innovators</div>
+            </div>
+            <div className='card -mt-[10vh] pt-6 pl-4 pr-6 bg-gray-200/90  transition-transform rounded-xl hover:scale-105 hover:shadow-2xl h-[55vh] w-[80vw] lg:w-[25vw]'>
+                <div className="image mb-4 h-[20vh] w-full grid place-items-center">
+                <Image src={dl} className='h-[20vh] w-[25vh]'/>
+                </div>
+                <div className='heading  font-bold space text-3xl mb-6'>Dates</div>
+                <div className='content text-xl'>Send a short video <b>3mins (max)</b> to <b>ablest@sastra.ac.in</b> by <b>20th sep 2023</b></div>
+            </div>
+            <div className='card -mt-[10vh] pt-6 pl-4 pr-6 bg-gray-200/90  transition-transform rounded-xl hover:scale-105 hover:shadow-2xl h-[55vh] w-[80vw] lg:w-[25vw]'>
+                <div className="image mb-4 h-[20vh] w-full grid place-items-center">
+                <Image src={star} className='h-[20vh] w-[25vh]'/>
+                </div>
+                <div className='heading  font-bold space text-3xl mb-6'>Awards</div>
+                <div className='content text-xl'>2 ideas will be awarded <b>Rs. 25000</b> each by <b>Janonam Natural Nutrition</b></div>
+            </div>
+          </div>
+          <div className="bg-orange-400 h-[150vh] lg:h-[60vh] w-full absolute -z-10 bottom-0 left-0 pb-82"></div>
         </div>
-        
-        
 
 
         {/* ideas */}
-        <div id='idea' className='pt-8 bg-gradient-to-bl from-rose-200 flex space-y-4 flex-col justify-center items-center md:pb-12 pb-8  px-2'>
+        <div id='idea' className='pt-36 lg:pt-8 bg-gradient-to-bl from-rose-200 flex space-y-4 flex-col justify-center items-center md:pb-12 pb-8  px-2'>
           <div className='text-4xl font-bold flex flex-row'><span>Ideas</span> <Image className='w-16' src={idea} /></div>
           <div className='flex flex-col justify-center items-center space-y-2 md:w-3/4 rounded-lg shadow-md p-8 bg-violet-500 text-white'>
             <div className='text-2xl font-bold'> 1.“30 different plants a week” challenge :</div>
@@ -116,7 +144,7 @@ export default function Home() {
         {/* dates awards */}
 
 
-        <div id='awa' className='flex flex-col justify-center items-center space-y-12 pb-12  md:m-0 bg-gradient-to-tl from-amber-300'>
+        {/* <div id='awa' className='flex flex-col justify-center items-center space-y-12 pb-12  md:m-0 bg-gradient-to-tl from-amber-300'>
           <div className='text-center text-4xl mt-12 font-bold'>Dates And Awards</div>
           <div className=' flex flex-col-reverse md:flex-row p-2 md:px-12 justify-center items-center md:w-3/5 '>
             <div className='shadow-lg  flex flex-col bg-violet-500 p-6 rounded-xl text-white'>
@@ -138,7 +166,7 @@ export default function Home() {
             </div>
 
           </div>
-        </div>
+        </div> */}
 
 
 
@@ -146,8 +174,6 @@ export default function Home() {
         <div className="pb-16  bg-gray-800 pt-8 ">
           <div className='text-2xl m-4 text-center text-gray-400'>Our Sponsors</div>
           <div className='flex md:flex-row flex-col flex-wrap justify-evenly items-center space-y-4 p-4'>
-            <Image className='h-24 mt-4' src={sas} />
-            <Image className='h-24' src={tbi} />
             <Image className='h-24' src={jana} />
           </div>
 
