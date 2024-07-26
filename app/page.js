8c0301072cd7c7a1b28e0979e7629f72ddc38b44
Syed Arshad <syedarshad80077@gmail.com>
2024-07-26 16:36:59 +0530

    Changed content

--- app/page.js
+++ app/page.js
@@ -51,8 +51,8 @@ export default function Home() {
         <div className='flex justify-center item-center flex-col sm:px-4 md:pt-8 md:px-12 pt-52 bg-gradient-to-tr from-green-300 h-[80vh] md:h-[84vh]'>
           <div className='flex md:flex-row flex-col justify-center items-center '>
             <div className='md:w-1/3 flex flex-col space-y-4 md:space-y-2 '>
-              <div className='md:text-6xl md:text-left text-center lg:text-6xl  text-3xl md:ml-0 ml-4 font-bold mt-12'>AgriTech Convergence Hackathon <br></br> 2024</div>
-              <div className='md:text-3xl  lg:text-3xl  text-xl text-center md:text-left md:ml-0 ml-4'>Seeking creative theme-oriented ideas from creators throughout India</div>
+              <div className='md:text-6xl md:text-left text-center lg:text-6xl  text-3xl md:ml-0 ml-4 font-bold mt-12'>Agri-TechHackathon <br></br> 2024</div>
+              <div className='md:text-3xl  lg:text-3xl  text-xl text-center md:text-left md:ml-0 ml-4'>Uniting innovative thinkers nationwide to create groundbreaking solutions for the critical challenges in today's agricultural landscape</div>
               <div className='md:py-4  py-4 md:ml-0 ml- flex md:justify-start justify-center'>
                 <button className='px-4 mb-20 md:px-6 lg:px-8 p-2 rounded-3xl flex text-xl md:text-xl flex-row justify-center items-center space-x-2 hover:bg-green-400 bg-green-500'><span><a id='details' href='https://docs.google.com/forms/d/e/1FAIpQLScU5zi0XvGaNRQvhmq2KWqV0d8gO46SgJHHK9uyvgUVtr5opQ/viewform' target='_main'>Register</a></span> <Image className='w-4' src={arrow} /></button>
               </div>
@@ -78,10 +78,10 @@ export default function Home() {
               <div className='heading  font-bold space text-3xl mb-6 text-center'>Eligibility</div>
               <div className='content text-xl'>
                 <ul className='list-disc pl-6'>
-                  <li>👨Students</li>
-                  <li>👨‍🎓Research Scholars</li>
-                  <li>👨‍💼Entrepreneur</li>
-                  <li>🛠Innovators</li>
+                  <li>👨Students and professionals from all backgrounds (technology, agriculture, business, etc.)</li>
+                  <li>👨‍🎓Researchers and academicians</li>
+                  <li>👨‍💼Entrepreneurs</li>
+                  <li>🛠Anyone with a passion for Agri-tech innovation</li>
                 </ul>
               </div>
             </div>
@@ -95,7 +95,7 @@ export default function Home() {
                   <li>📜Registration is mandatory</li>
                   <li>🧑‍🤝‍🧑Each team can have up to 4 members, including the team leader</li>
                   <li>🎥 Follow the guidelines for your pre-recorded presentation given in the form</li>
-                  <li>📆Deadline - August 17th, 2024</li>
+                  <li>📆Presentation submission Deadline - August 17th, 2024</li>
                  {/* <li>📧Submit at <b>ablest@sastra.ac.in</b></li> */} 
                 </ul>
               </div>
@@ -121,25 +121,25 @@ export default function Home() {
 
         {/* ideas */}
         <div id='idea' className='abi_bhu pt-8 bg-gradient-to-bl from-yellow-200 flex space-y-4 flex-col justify-center items-center md:pb-12 pb-8  px-2'>
-          <div className='text-4xl font-bold flex flex-row'><span>Ideas</span> <Image className='w-16' src={idea} /></div>
+          <div className='text-4xl font-bold flex flex-row'><span>Challenge Themes</span> <Image className='w-16' src={idea} /></div>
 
-          {/*
+          
           <div className='flex flex-col justify-center items-center space-y-2 md:w-3/4 rounded-lg shadow-md p-8 bg-violet-500 text-white'>
-             <div className='text-2xl w-full text-left md:ml-32 font-bold'> 1.“30 different plants a week” challenge-Balancing millets :   </div>
-            <div className='md:w-4/5 text-xl'>
-              <ul className='list-disc'>
+             <div className='text-2xl w-full md:ml-32 font-bold text-center'> 1.Sodic Soil and Saline Water Management </div>
+            {/* <div className='md:w-4/5 text-xl'>
+               <ul className='list-disc'>
                 <li>Design an ideal meal (with no anti-nutrients) with at least 30 different plants (plant parts) & millets (compulsorily at least 3) to meet 50% of recommended RDA (Recommended Daily Allowances of nutrients) for Indian adults</li>
                 <li>Offer convenience in consumption that should be ready to eat or ready to drink – straight out of the box / can – without any cooking/ heating etc</li>
                 <li>Enable versatile culinary profile to prepare a wide range of dishes and snacks. Embracing millets in modern diets that appeal to modern tastes and dietary preferences</li>
-              </ul>
+              </ul> 
 
 
                ---- already commented ---  a. Design an ideal meal (with no anti-nutrients) with at least 30 different plants (plant parts) & millets (compulsorily at least 3) to meet 50% of recommended RDA (Recommended Daily Allowances of nutrients) for Indian adults.<br></br> b. Offer convenience in consumption that should be ready to eat or ready to drink – straight out of the box / can – without any cooking/ heating etc.<br></br> c. Enable versatile culinary profile to prepare a wide range of dishes and snacks. Embracing millets in modern diets that appeal to modern tastes and dietary preferences. 
-            </div> 
+            </div>  */}
           </div>
           <div className='flex flex-col justify-center items-center space-y-2 md:w-3/4 rounded-lg shadow-md p-8 bg-green-500 text-white'>
-             <div className='text-2xl w-full text-left md:ml-32 font-bold'> 2. Design Food-Ink cartridge & food printer for Jananom’s vegan Nutrimix :</div>
-            <div className='md:w-4/5 text-xl'>
+             <div className='text-2xl w-full text-left md:ml-32 font-bold text-center'>2.Value Additions in Food Products (specific to processing techniques)</div>
+            {/* <div className='md:w-4/5 text-xl'>
               <ul className='list-disc'>
                 <li>Formulate it suitably for flowing characteristics & rheology for printing</li>
                 <li>Able to customize food design on the printer – from a mobile app / website</li>
@@ -148,11 +148,11 @@ export default function Home() {
                 
                 
                 ---- already commented---- a. Formulate it suitably for flowing characteristics & rheology for printing.<br></br> b. Able to customize food design on the printer – from a mobile app / website.<br></br> c. Total printing time not to exceed 180 minutes per dish. 
-            </div> 
+            </div>  */}
           </div>
           <div className='flex flex-col justify-center items-center space-y-2 md:w-3/4 rounded-lg shadow-md p-8 bg-orange-400 text-white'>
-            { /* <div className='text-2xl w-full text-left md:ml-32 font-bold'>3. Gut-brain hack for yogic meal :</div>
-            <div className='md:w-4/5 text-xl'>
+          <div className='text-2xl w-full text-left md:ml-32 font-bold text-center'>3.Farm Mechanization</div>
+            {/* <div className='md:w-4/5 text-xl'>
               <ul className='list-disc'>
                 <li>As per Indian traditional system, there are certain recommended diets that support gut-brain axis for those who want to do meditation and do tasks that involve mental focus. Design such a diet</li>
                 <li>Should meet atleast 20% of RDA</li>
@@ -161,8 +161,53 @@ export default function Home() {
                 
                 
                 ----already commented---- a. As per Indian traditional system, there are certain recommended diets that support gut-brain axis for those who want to do meditation and do tasks that involve mental focus. Design such a diet.<br></br> b. Should meet atleast 20% of RDA<br></br> c. Should involve minimal cooking.\ 
-            </div> 
-          </div> */}
+            </div>  */}
+
+          </div> 
+          <div className='flex flex-col justify-center items-center space-y-2 md:w-3/4 rounded-lg shadow-md p-8 bg-violet-500 text-white'>
+             <div className='text-2xl w-full md:ml-32 font-bold text-center'>4.Issues to Resolve Challenges Faced by Differently Abled</div>
+            {/* <div className='md:w-4/5 text-xl'>
+               <ul className='list-disc'>
+                <li>Design an ideal meal (with no anti-nutrients) with at least 30 different plants (plant parts) & millets (compulsorily at least 3) to meet 50% of recommended RDA (Recommended Daily Allowances of nutrients) for Indian adults</li>
+                <li>Offer convenience in consumption that should be ready to eat or ready to drink – straight out of the box / can – without any cooking/ heating etc</li>
+                <li>Enable versatile culinary profile to prepare a wide range of dishes and snacks. Embracing millets in modern diets that appeal to modern tastes and dietary preferences</li>
+              </ul> 
+
+
+               ---- already commented ---  a. Design an ideal meal (with no anti-nutrients) with at least 30 different plants (plant parts) & millets (compulsorily at least 3) to meet 50% of recommended RDA (Recommended Daily Allowances of nutrients) for Indian adults.<br></br> b. Offer convenience in consumption that should be ready to eat or ready to drink – straight out of the box / can – without any cooking/ heating etc.<br></br> c. Enable versatile culinary profile to prepare a wide range of dishes and snacks. Embracing millets in modern diets that appeal to modern tastes and dietary preferences. 
+            </div>  */}
+          </div>
+          
+
+
+          <div className='flex flex-col justify-center items-center space-y-2 md:w-3/4 rounded-lg shadow-md p-8 bg-green-500 text-white'>
+             <div className='text-2xl w-full text-left md:ml-32 font-bold text-center'>5.Climate Smart Agriculture</div>
+            {/* <div className='md:w-4/5 text-xl'>
+              <ul className='list-disc'>
+                <li>Formulate it suitably for flowing characteristics & rheology for printing</li>
+                <li>Able to customize food design on the printer – from a mobile app / website</li>
+                <li>Total printing time not to exceed 180 minutes per dish</li>
+              </ul>
+                
+                
+                ---- already commented---- a. Formulate it suitably for flowing characteristics & rheology for printing.<br></br> b. Able to customize food design on the printer – from a mobile app / website.<br></br> c. Total printing time not to exceed 180 minutes per dish. 
+            </div>  */}
+          </div>
+          <div className='flex flex-col justify-center items-center space-y-2 md:w-3/4 rounded-lg shadow-md p-8 bg-orange-400 text-white'>
+          <div className='text-2xl w-full text-left md:ml-32 font-bold text-center'>6.Other Areas Related to Agriculture (if any)</div>
+            {/* <div className='md:w-4/5 text-xl'>
+              <ul className='list-disc'>
+                <li>As per Indian traditional system, there are certain recommended diets that support gut-brain axis for those who want to do meditation and do tasks that involve mental focus. Design such a diet</li>
+                <li>Should meet atleast 20% of RDA</li>
+                <li>Should involve minimal cooking</li>
+              </ul>
+                
+                
+                ----already commented---- a. As per Indian traditional system, there are certain recommended diets that support gut-brain axis for those who want to do meditation and do tasks that involve mental focus. Design such a diet.<br></br> b. Should meet atleast 20% of RDA<br></br> c. Should involve minimal cooking.\ 
+            </div>  */}
+
+          </div> 
+
           <h1 className='text-4xl text-center'>Things will be revealed soon 🤩</h1>
         </div>
 
